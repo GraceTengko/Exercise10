f2b6d0e477c7d97ba3f867958e4819e9ba22fc1f
14grth <113334647+14grth@users.noreply.github.com>
2023-11-24 15:22:47 +0800

    Add files via upload

--- GroupExercise8/src/components/header/generations/gen1/index.jsx
+++ GroupExercise8/src/components/header/generations/gen1/index.jsx
@@ -1,9 +1,10 @@
-const Gen_1 = () => {
+const Gen_1 = (props) => {
+  const { generationNumber} = props;
     return (
          
-         <h1>Gen_1</h1>
+         <h1>{'Gen_1</h1 - Generation ${generationNumber}'}</h1>
   
       
-      )
+      );
     }
-  export default Gen_1
+  export default Gen_1;
